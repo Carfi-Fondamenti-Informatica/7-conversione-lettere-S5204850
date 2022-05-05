9eaa6e75b627f80a6a9fa9eb5ae5121695a0e755
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-05-05 15:57:24 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+bool isChar(char &myChar){
+    if(myChar >= 65 && myChar <= 90){
+        myChar = char(myChar + 32);
+        return true;
+    }
+
+    if(myChar >=97 && myChar <= 122){
+        myChar = char(myChar - 32);
+        return true;
+    }
+    return false;
+}
