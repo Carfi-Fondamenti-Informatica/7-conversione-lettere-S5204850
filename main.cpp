3fbd35c3c1e797ae83e73420f7f7373955c9f3da
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-05-06 13:31:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main() {
     char input;
-    cout<<"Inserisci qualcosa: ";
     cin >> input;
     if(funzione (input)) cout <<"Vero"<< input << endl;
     else cout << "errore"<<endl;    return 0;
