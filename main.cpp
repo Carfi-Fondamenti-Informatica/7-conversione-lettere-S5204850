bab9041e90826af8fff7314c985234cce2685c87
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-05-06 13:23:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     char input;
     cin >> input;
-    cout << endl;
     if(isChar(input)) cout <<"Vero: "<< input << endl;
     else cout << "errore"<<endl;
     return 0;
