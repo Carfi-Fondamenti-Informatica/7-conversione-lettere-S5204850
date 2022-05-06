ae7761e32b1e529bef0db4a5d010005bcba69d98
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-05-06 12:47:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-
+#include "lib.cpp"
+using namespace std;
 int main() {
     char input;
     cout<<"Inserisci qualcosa: ";
     cin >> input;
     cout << endl;
     if(isChar(input)) cout <<"Vero: "<< input << endl;
-    else cout << "Falso"<<endl;
+    else cout << "errore"<<endl;
     return 0;
 }
 
