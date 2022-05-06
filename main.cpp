390090b94782f47e5dec4f9197506a535ccca14b
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-05-06 15:19:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include "lib.cpp"
-
+#include "lib.h"
 using namespace std;
 
 int main() {
-    char input;
-    cin >> input;
-    if(funzione (input)) cout <<"Vero"<< input << endl;
-    else cout << "errore"<<endl;    return 0;
+    char c;
+    cin >> c;
+    int n=(int)c;
+    bool x=caratteri(n);
+    
+    if(x==1){
+        if ((65<= n) && (n<=90)){
+            n=n+32;
+            cout <<(char)n<< endl;
+        } else if ((97<=n) && (n<=122)){
+            n=n-32;
+            cout << (char(n)<<endl; 
+                     }
+                     }
+                     else {
+                         cout <<"errore"<<endl;
+                     }
+                    return 0;
 }
 
